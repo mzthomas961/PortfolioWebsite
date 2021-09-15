fdbfa8c5d6824c2ed613d0016d7c4188eb9f7e70
Matthew Thomas <mzthomas96@gmail.com>
2021-09-14 21:23:50 -0400

    working on project Cards

--- src/Projects.js
+++ src/Projects.js
@@ -1,21 +1,47 @@
 import React from 'react';
+import ProjectCard from './ProjectCard';
 
 function Projects(){
+    const data = [
+        {
+        "id":1, 
+        "title": "Ruby CLI",
+        "stack": "Ruby",
+        "Github": "https://github.com/mzthomas961/Ruby-CLI"
+    },
+    {
+        "id":2, 
+        "title": "Transparent Travel",
+        "stack": "Ruby on Rails",
+        "Github": "https://github.com/mzthomas96/TransparentTravel"
+    },
+    {
+        "id":3, 
+        "title": "Bean Tracker",
+        "stack": "Javascript and Ruby on Rails",
+        "Github": "https://github.com/mzthomas961/BeanTracker-Frontend(Frontend) + https://github.com/mzthomas961/BeanTracker-Backend(Backend)"
+    },
+    {
+        "id":4, 
+        "title": "Git Right Kitchen",
+        "stack": "React and Ruby on Rails",
+        "Github": "https://github.com/mzthomas961/Git-Right-Recipes-Frontend(Frontend)+ https://github.com/mzthomas961/Git-Right-Recipes-Backend(Backend)"
+    }
+
+    ]
+    const projectObjects = data.map((project) => {
+        return(
+            <ProjectCard
+            key = {project.id}
+            title = {project.title}
+            stack = {project.stack}
+            github = {project.github}
+            />
+        )
+    })
+
     return (
-        <ul>  
-            <li><strong>Feed Your Neighbor</strong>
-            <p>A social media app designed to trade food with others.
-                I was interested in creating a way for people to come<br />
-                together and share what they loved to eat!</p></li>
-            <li><strong>Git Right Kitchen</strong>
-            <p>A recipe sharing app.</p>
-            <p>Technologies used: <br />
-            - React <br />
-            -RoR <br />
-            -HTML + CSS <br />
-            </p>
-            </li>
-        </ul>
+       <div>{projectObjects}</div>
     )
 }
 export default Projects
