de0c57718daa8aa976ded08828c69d8485676304
Matthew Thomas <mzthomas96@gmail.com>
2022-05-27 15:32:30 -0400

    changed bg color

--- src/Bio.js
+++ src/Bio.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Bio(){
     return(
         <div className="bio" >
-    <p className="bioText"> Hey there, welcome to my page. I'm an aspiring software engineer,aand a recent graduate of Flatiron School. I'm proficient in React, Ruby on Rails, Python and Javascript.
+    <p className="bioText"> welcome to my page. I'm an aspiring software engineer,aand a recent graduate of Flatiron School. I'm proficient in React, Ruby on Rails, Python and Javascript.
                             I invite you to check out my <a href="/resume.pdf" target="_blank"> resume</a> or explore my work. If you'd like to speak,
                             don't afraid to <a href = "mailto: mzthomas96@gmail.com">reach out.</a>
 
